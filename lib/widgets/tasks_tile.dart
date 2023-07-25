adf7a8f6644878f7dbba0960cc47b83a6691b452
ForsythIshan <ishand1005@gmail.com>
2023-07-26 01:49:28 +0530

    Managed states to apply strikethrough

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 
+// Each task tile, consisting of of task detail and checkbox
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool? isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("This is a task"),
-      trailing: Checkbox(value: false, onChanged: (condition) {}),
+      title: Text(
+        "This is a task",
+        style: TextStyle(
+            decoration: isChecked == true ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(
+        checkBoxState: isChecked,
+        toggleCheckboxState: (bool? checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
+      ),
     );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox(
+      {this.checkBoxState, required this.toggleCheckboxState, super.key});
+
+  final bool? checkBoxState;
+  final Function(bool?) toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: checkBoxState, onChanged: toggleCheckboxState);
   }
 }
